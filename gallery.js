bf15a64ab0a539371fa1439f2414648e190cfca1
AnnaAardvark <atamlin@uwaterloo.ca>
2026-07-08 00:42:10 -0400

    Refine personal website - add more pages and content to reflect experience and interests

---
+++ gallery.js
@@ -0,0 +1,336 @@
+const photoTripLog = [
+    {
+        src: "assets/plainsofabraham.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | PLAINS OF ABRAHAM, QUÉBEC CITY, CANADA | ISO 100 | f/6.5 | August 2025"
+    },
+        {
+        src: "assets/quebeccity.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ÎLE D'ORLÉANS, QUÉBEC, CANADA | ISO 100 | f/4.3 | August 2025"
+    },
+    {
+        src: "assets/umbrellas.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | UMBRELLA ALLEY, QUÉBEC CITY, CANADA | ISO 100 | f/4.3 | August 2025"
+    },
+    {
+        src: "assets/ago1.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | YAYOI KUSAMA'S INFINITY MIRRORS, ART GALLERY OF ONTARIO, CANADA | ISO 100 | f/3.0 | September 2025"
+    },
+    {
+        src: "assets/ago2.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ART GALLERY OF ONTARIO, CANADA | ISO 320 | f/3.0 | September 2025"
+    },
+    {
+        src: "assets/plane.jpg",
+        metadata: "IPHONE 15 | FLYING OVER MOUNT FUJI, JAPAN | ISO 40 | f/1.6 | January 2026"
+    },
+    {
+        src: "assets/chinatown.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | CHINATOWN, SINGAPORE | ISO 160 | f/3.0 | January 2026"
+    },
+    {
+        src: "assets/marinabaysands.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | MARINA BAY SANDS, SINGAPORE | ISO 100 | f/5.5 | January 2026"
+    },
+    {
+        src: "assets/supertrees.jpg",
+        metadata: "IPHONE 15 | SUPERTREE GROVE, SINGAPORE | ISO 500 | f/1.6 | January 2026"
+    },
+    {
+        src: "assets/clarkequay.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | CLARKE QUAY, SINGAPORE | ISO 100 | f/4.3 | January 2026"
+    },
+    {
+        src: "assets/chiangmaiguide.jpg",
+        metadata: "IPHONE 15 | KEW MAE PAN NATURE TRAIL, THAILAND | ISO 50 | f/1.6 | January 2026"
+    },
+    {
+        src: "assets/chiangmaisunset.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | CHIANG MAI SUNSET, THAILAND | ISO 400 | f/1.6 | January 2026"
+    },
+    {
+        src: "assets/chiangrai.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | WAT RONG KHUN, THAILAND | ISO 100 | f/4.3 | January 2026"
+    },
+    {
+        src: "assets/longneckvillage.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | LONG NECK KAREN VILLAGE, THAILAND | ISO 100 | f/3.0 | January 2026"
+    },
+    {
+        src: "assets/penang.jpg",
+        metadata: "IPHONE 15 | KEK LOK SI TEMPLE, MALAYSIA | ISO 50 | f/1.6 | February 2026"
+    },
+    {
+        src: "assets/jiufen.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | JIUFEN, TAIWAN | ISO 200 | f/3.0 | February 2026"
+    },
+    {
+        src: "assets/spiderweb.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | QINGTIANGANG GRASSLAND, TAIWAN | ISO 100 | f/3.0 | February 2026"
+    },
+    {
+        src: "assets/foggymountain.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | QINGTIANGANG GRASSLAND, TAIWAN | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/snoopylight.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | TAICHUNG, TAIWAN | ISO 100 | f/7.0 | February 2026"
+    },
+    {
+        src: "assets/alishan.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ALISHAN NATIONAL FOREST RECREATION AREA, TAIWAN | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/foggytrees.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ALISHAN NATIONAL FOREST RECREATION AREA, TAIWAN | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/cherryblossoms.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ERYANPING TEA TRAIL, TAIWAN | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/alishanstairs.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ALISHAN, TAIWAN | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/sunmoonlake.jpg",
+        metadata: "IPHONE 15 | SUN MOON LAKE, TAIWAN | ISO 50 | f/1.6 | February 2026"
+    },
+    {
+        src: "assets/lionsrockhike.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | LION ROCK HIKE, HONG KONG | ISO 100 | f/4.3 | February 2026"
+    },
+    {
+        src: "assets/hanoi.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | HANOI OLD QUARTER, VIETNAM | ISO 200 | f/3.0 | March 2026"
+    },
+    {
+        src: "assets/ninhbinh.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | NINH BINH, VIETNAM | ISO 100 | f/4.3 | March 2026"
+    },
+    {
+        src: "assets/angkorwat2.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ANGKOR WAT, CAMBODIA | ISO 100 | f/5.5 | March 2026"
+    },
+    {
+        src: "assets/angkorwat.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ANGKOR WAT, CAMBODIA | ISO 100 | f/6.5 | March 2026"
+    },
+    {
+        src: "assets/cambodia.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | SIEM REAP, CAMBODIA | ISO 100 | f/4.3 | March 2026"
+    },
+    {
+        src: "assets/angkorwat3.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ANGKOR WAT, CAMBODIA | ISO 100 | f/6.5 | March 2026"
+    },
+    {
+        src: "assets/elephantsanctuary.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | ELEPHANT SANCTUARY, CAMBODIA | ISO 100 | f/5.5 | March 2026"
+    },
+    {
+        src: "assets/jadesnowmountain.jpg",
+        metadata: "IPHONE 15 | YULONG SNOW MOUNTAIN, CHINA | ISO 1000 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/jadesnowmountainsummit.jpg",
+        metadata: "SONY CYBERSHOT DSC-W310 | YULONG SNOW MOUNTAIN (4860 M AMSL), CHINA | ISO 100 | f/4.3 | March 2026"
+    },
+    {
+        src: "assets/bluemoonvalley.jpg",
+        metadata: "IPHONE 15 | BLUE MOON VALLEY, CHINA | ISO 50 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/impressionlijiang.jpg",
+        metadata: "IPHONE 15 | IMPRESSION LIJIANG THEATRE, CHINA | ISO 50 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/oldtownsunset.jpg",
+        metadata: "IPHONE 15 | OLD TOWN OF LIJIANG, CHINA | ISO 25 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/tigerleapinggorge.jpg",
+        metadata: "IPHONE 14 | TIGER LEAPING GORGE, CHINA | ISO 50 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/tigerleapinggorge3.jpg",
+        metadata: "IPHONE 15 | TIGER LEAPING GORGE, CHINA | ISO 50 | f/2.4 | March 2026"
+    },
+    {
+        src: "assets/tigerleapinggorge2.jpg",
+        metadata: "IPHONE 15 | TIGER LEAPING GORGE, CHINA | ISO 50 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/tigerleapinggorge4.jpg",
+        metadata: "IPHONE 15 | TIGER LEAPING GORGE, CHINA | ISO 25 | f/2.4 | March 2026"
+    },
+    {
+        src: "assets/mahjong.jpg",
+        metadata: "IPHONE 15 | KUNMING, CHINA | ISO 125 | f/1.6 | March 2026"
+    },
+    {
+        src: "assets/lyingbuddha.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | RECLINING BUDDHA, THAILAND | ISO 100 | f/2.7 | April 2026"
+    },
+    {
+        src: "assets/grandpalace.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | GRAND PALACE, THAILAND | ISO 80 | f/13.0 | April 2026"
+    },
+    {
+        src: "assets/watarun.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | WAT ARUN, THAILAND | ISO 80 | f/2.7 | April 2026"
+    },
+    {
+        src: "assets/railaybeach.jpg",
+        metadata: "IPHONE 15 | RAILAY BEACH, THAILAND | ISO 50 | f/1.6 | April 2026"
+    },
+    {
+        src: "assets/monkeys.jpg",
+        metadata: "IPHONE 15 | MISCHEVIOUS MONKEYS, THAILAND | ISO 40 | f/1.6 | April 2026"
+    },
+    {
+        src: "assets/giantclam.jpg",
+        metadata: "OLYMPUS TG-7 | OUTER GREAT BARRIER REEF (PORT DOUGLAS), AUSTRALIA | ISO 100 | f/2.8 | April 2026"
+    },
+    {
+        src: "assets/seaturtle.jpg",
+        metadata: "OLYMPUS TG-7 | SEA TURTLE, AUSTRALIA | ISO 100 | f/8.0 | April 2026"
+    },
+    {
+        src: "assets/clownfish.jpg",
+        metadata: "OLYMPUS TG-7 | FINDING NEMO, AUSTRALIA | ISO 100 | f/2.8 | April 2026"
+    },
+    {
+        src: "assets/coralnursery.jpg",
+        metadata: "OLYMPUS TG-7 | CORAL NURSERY, AUSTRALIA | ISO 100 | f/8.0 | April 2026"
+    },
+    {
+        src: "assets/snorkelling.jpg",
+        metadata: "OLYMPUS TG-7 | OPAL REEF, AUSTRALIA | ISO 100 | f/2.8 | April 2026"
+    },
+    {
+        src: "assets/kangaroos.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | KANGAROOS AT LONE PINE KOALA SANCTUARY, AUSTRALIA | ISO 80 | f/4.5 | April 2026"
+    },
+    {
+        src: "assets/rainbowlorikeets.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | RAINBOW LORIKEETS AT LONE PINE KOALA SANCTUARY, AUSTRALIA | ISO 80 | f/2.8 | April 2026"
+    },
+    {
+        src: "assets/sydneyoperahouse.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | SYDNEY OPERA HOUSE, AUSTRALIA | ISO 80 | f/9.0 | April 2026"
+    },
+    {
+        src: "assets/spitcoast.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | MANLY, AUSTRALIA | ISO 80 | f/10.0 | April 2026"
+    },
+    {
+        src: "assets/manlycoast.jpg",
+        metadata: "IPHONE 15 | SPIT, AUSTRALIA | ISO 20 | f/1.9 | April 2026"
+    },
+    {
+        src: "assets/jrtokyo.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | JR EAST MAP, JAPAN | ISO 80 | f/2.7 | April 2026"
+    },
+    {
+        src: "assets/temple.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | ENOSHIMA, JAPAN | ISO 80 | f/2.7 | May 2026"
+    },
+    {
+        src: "assets/enoden.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | ENODEN, JAPAN | ISO 80 | f/9.0 | May 2026"
+    },
+    {
+        src: "assets/enoshima.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | ENOSHIMA, JAPAN | ISO 80 | f/13.0 | May 2026"
+    },
+    {
+        src: "assets/monorail.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | SHONAN MONORAIL, JAPAN | ISO 80 | f/8.0 | May 2026"
+    },
+    {
+        src: "assets/goldenhourfuji.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | MOUNT FUJI DURING GOLDEN HOUR, JAPAN | ISO 80 | f/5.7 | May 2026"
+    },
+    {
+        src: "assets/pinkfuji.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | SUNSET OVER MOUNT FUJI, JAPAN | ISO 250 | f/4.5 | May 2026"
+    },
+    {
+        src: "assets/train.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | ENODEN, JAPAN | ISO 200 | f/2.7 | May 2026"
+    },
+    {
+        src: "assets/transitmuseum.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | RAILWAY MUSEUM IN SAITAMA, JAPAN | ISO 400 | f/2.7 | May 2026"
+    },
+    {
+        src: "assets/baegundaepeak.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | BAEGUNDAE PEAK, SOUTH KOREA | ISO 80 | f/4.7 | May 2026"
+    },
+    {
+        src: "assets/bukhansan.jpg",
+        metadata: "IPHONE 15 | BUKHANSAN NATIONAL PARK, SOUTH KOREA | ISO 80 | f/8.0 | May 2026"
+    },
+    {
+        src: "assets/bukhansantrail.jpg",
+        metadata: "IPHONE 15 | BUKHANSAN NATIONAL PARK, SOUTH KOREA | ISO 25 | f/4.7 | May 2026"
+    },
+    {
+        src: "assets/hanbok.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | BUKCHON HANOK VILLAGE, SOUTH KOREA | ISO 80 | f/4.7 | May 2026"
+    },
+    {
+        src: "assets/seoul.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | SEOUL → INCHEON, SOUTH KOREA | ISO 400 | f/1.6 | May 2026"
+    },
+    {
+        src: "assets/guangzhou.jpg",
+        metadata: "IPHONE 15 | GUANGZHOU, CHINA | ISO 50 | f/1.6 | May 2026"
+    },
+    {
+        src: "assets/drsunyatsenmemorial.jpg",
+        metadata: "IPHONE 15 | DR. SUN YAT SEN'S MEMORIAL HALL, CHINA | ISO 50 | f/1.6 | May 2026"
+    },
+    {
+        src: "assets/cantontower.jpg",
+        metadata: "IPHONE 15 | CANTON TOWER, CHINA | ISO 50 | f/1.6 | May 2026"
+    },
+    {
+        src: "assets/rcharris.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | R.C. HARRIS WATER TREATMENT PLANT, TORONTO, CANADA | ISO 80 | f/10.0 | May 2026"
+    },
+    {
+        src: "assets/rcharris1.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | R.C. HARRIS WATER TREATMENT PLANT, TORONTO, CANADA | ISO 80 | f/2.8 | May 2026"
+    },
+    {
+        src: "assets/rcharris2.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | R.C. HARRIS WATER TREATMENT PLANT, TORONTO, CANADA | ISO 80 | f/2.7 | May 2026"
+    },
+    {
+        src: "assets/toronto.jpg",
+        metadata: "SONY CYBERSHOT DSC-W320 | TORONTO, CANADA | ISO 80 | f/8.0 | May 2026"
+    }
+];
+
+let currentIndex = 0;
+
+function changePhoto(direction) {
+    currentIndex += direction;
+    
+    if (currentIndex >= photoTripLog.length) currentIndex = 0;
+    if (currentIndex < 0) currentIndex = photoTripLog.length - 1;
+    
+    const photoElement = document.getElementById("active-screen-photo");
+    const metadataElement = document.getElementById("photo-metadata");
+    
+    photoElement.style.opacity = 0.3;
+    
+    setTimeout(() => {
+        photoElement.src = photoTripLog[currentIndex].src;
+        
+        metadataElement.innerText = photoTripLog[currentIndex].metadata;
+        
+        photoElement.style.opacity = 1;
+    }, 120);
+}
